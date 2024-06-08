2ba3aa9d966cd17927d55e6b2fffc87c5a52114d
draysongz <dharmiegra4@gmail.com>
2024-06-08 09:20:20 +0100

    dd

--- src/components/Dash.jsx
+++ src/components/Dash.jsx
@@ -1,7 +1,14 @@
-import { Card, CardBody, Flex, Text, Box, HStack, Icon } from '@chakra-ui/react';
+import { Card, CardBody, Flex, Text, Box, HStack, Icon , Tooltip,
+    useColorModeValue,
+    useBreakpointValue,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { GiTrophyCup } from "react-icons/gi";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight, MdSpaceDashboard, MdGroups } from "react-icons/md";
+import { IoMdStats } from "react-icons/io";
+import { SiGoogletasks } from "react-icons/si";
+import { FaFireAlt } from "react-icons/fa";
+import NextLink from 'next/link';
 
 const Dash = () => {
   const [isScaled, setIsScaled] = useState(false);
@@ -9,6 +16,14 @@ const Dash = () => {
   const handleImageClick = () => {
     setIsScaled(!isScaled);
   };
+
+  const navData = [
+     { icon: MdGroups, title: "Ref", link: "/referrals" },
+    {icon : SiGoogletasks, title: 'Tasks', link: '/task'},
+    { icon: MdSpaceDashboard, title: "Tap", link: "/" },
+    {icon: FaFireAlt, title:'Boost', link: '/boost'},
+    {icon: IoMdStats, title: 'stats' , link: '/stats'}
+  ];
 
   return (
     <Flex minH="100vh" bgColor="#10171d" direction="column" alignItems="center" justifyContent="center">
@@ -34,6 +49,44 @@ const Dash = () => {
           cursor="pointer"
         />
       </Flex>
+
+
+      <Flex
+          position="fixed"
+          bottom={0}
+          left={0}
+          right={0}
+          
+          color={useColorModeValue("#fff", "#fff")}
+          p={3}
+          justifyContent="space-around"
+          zIndex={1}
+          display={useBreakpointValue({base: 'flex', md: 'flex', lg: 'none'})}
+          bgColor={'#251d33'}
+        >
+          {navData.map((item, index) => (
+            <Tooltip
+              
+              hasArrow={index == 1 ? true : false}
+              placement="top"
+            >
+              <Flex
+                key={item.title}
+                flexDir="column"
+                align="center"
+                as={NextLink}
+                href={item.link}
+              >
+                <Icon as={item.icon} boxSize={5} mb={2} />
+                <Text fontSize={{ base: "xs", md: "md" }}>{item.title}</Text>
+              </Flex>
+              
+            </Tooltip>
+          ))}
+
+          
+          
+        </Flex>
     </Flex>
   );
 };
